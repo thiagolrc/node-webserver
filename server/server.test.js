4898d0a9342dff41bbf1e448763fa90d13d7e1f5
Thiago Lourenconi <thiago.lourenconi@olxbr.com>
2018-08-07 11:44:17 -0300

    Grouping tests using 'describe'

--- server/server.test.js
+++ server/server.test.js
@@ -2,21 +2,23 @@ const request = require('supertest');
 
 var app = require('./server.js').app;
 
-it('should return hello world', (done) => {
-  request(app)
-    .get('/hello')
-    .expect(200)
-    .expect('Hello World!')
-    .end(done);
-});
+describe('Server Tests', () => {
+  it('should return hello world', (done) => {
+    request(app)
+      .get('/hello')
+      .expect(200)
+      .expect('Hello World!')
+      .end(done);
+  });
 
-it('should return json object', (done) => {
-  request(app)
-    .get('/json')
-    .expect(200)
-    .expect({
-      name: 'Thiago',
-      age: 19
-    })
-    .end(done);
+  it('should return json object', (done) => {
+    request(app)
+      .get('/json')
+      .expect(200)
+      .expect({
+        name: 'Thiago',
+        age: 19
+      })
+      .end(done);
+  });
 });
--- util/utils.test.js
+++ util/utils.test.js
@@ -1,33 +1,39 @@
 const utils = require('./utils.js');
 const expect = require('expect');
 
-it('should add two numbers', () => {
-  var res = utils.add(4,5);
+describe('Utils Tests', () => {
 
-  expect(res).toBe(9).toBeA('number');
-});
+  it('should add two numbers', () => {
+    var res = utils.add(4,5);
 
-it('should compare two objects', () => {
-  var obj = {
-    name: 'Thiago',
-    age: 19
-  };
-
-  expect(obj).toEqual({age:19, name:  'Thiago'});
-});
-
-it('should expect some values', () => {
-  var obj = {
-    name: 'Thiago',
-    age: 19
-  };
-
-  expect(obj).toInclude({age: 19});
-});
-
-it('should async add two numbers', (done) => {
-  utils.addAsync(4,5,(res) => {
     expect(res).toBe(9).toBeA('number');
-    done();
   });
+
+  it('should compare two objects', () => {
+    var obj = {
+      name: 'Thiago',
+      age: 19
+    };
+
+    expect(obj).toEqual({age:19, name:  'Thiago'});
+  });
+
+  it('should expect some values', () => {
+    var obj = {
+      name: 'Thiago',
+      age: 19
+    };
+
+    expect(obj).toInclude({age: 19});
+  });
+
+  describe('Utils Async Tests', () => {
+    it('should async add two numbers', (done) => {
+      utils.addAsync(4,5,(res) => {
+        expect(res).toBe(9).toBeA('number');
+        done();
+      });
+    });
+  });
+
 });
